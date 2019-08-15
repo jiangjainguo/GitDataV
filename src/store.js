538e91de26790a1aafbee4eba4ce1da817789d03
jiangjainguo <781612284@qq.com>
2019-08-15 10:26:15 +0800

    first commot

---
+++ src/store.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import Cookies from "js-cookie";
+import { getLanguage } from "@/lang/index";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        language: getLanguage()
+    },
+    mutations: {
+        SET_LANGUAGE: (state, language) => {
+            state.language = language;
+            console.log(state.language)
+            Cookies.set("language", language);
+        }
+    },
+    actions: {
+        setLanguage({ commit }, language) {
+            commit("SET_LANGUAGE", language);
+        }
+    }
+});
